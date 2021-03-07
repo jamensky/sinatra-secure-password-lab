a755b52e770b5b8988167ac685ebde7d25dd5232
Jamal <jamensky1991@gmail.com>
2021-03-07 15:09:22 +0000

    commit tree ad388b83dbd4ea93fe3a7bab35b33ce2de892809

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,11 +17,18 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
-    
+    user = User.new(username: params[:username], password: params[:password])
+     
+     if params[:username].length == 0 && user.save
+      #binding.pry
+       redirect '/login'
+     else 
+       redirect '/failure'
+     end 
+
   end
 
   get '/account' do
-    @user = User.find(session[:user_id])
     erb :account
   end
 
@@ -31,7 +38,15 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/login" do
-    ##your code here
+    user = User.find_by(username: params[:username])
+
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect '/account'
+    else 
+      redirect '/failure'
+    end 
+
   end
 
   get "/failure" do
--- db/migrate/20210306162832_DESKTOP-RBF0D2V.rb
+++
@@ -1,4 +0,0 @@
-class DESKTOP-RBF0D2V < ActiveRecord::Migration[5.1]
-  def change
-  end
-end
